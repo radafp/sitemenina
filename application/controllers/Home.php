27245e7da394516119d098753a08c4e468415a15
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-12-13 19:58:04 -0200

    alteracao do DB, carregando somente a regiao da $_SESSION

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -17,14 +17,15 @@ class home extends CI_Controller {
 
     public function balneario_camboriu() {
         $dados['viewName']          = 'balneario_camboriu';
-        $_SESSION['regiao']         = 'BC';
+        $_SESSION['regiao']         = 'bc';
+        $regiao                     = 'bc';
         $_SESSION['controller']     = 'balneario_camboriu';
-        $dados['titulo']            = $this->Novomenina->noticias_turistmo_destaque();
-        $dados['eventos']           = $this->Novomenina->eventos();
-        $dados['programacao']       = $this->Novomenina->programacao();
-        $dados['videos']            = $this->Novomenina->videos();
-        $dados['titulo_jornalismo'] = $this->Novomenina->titulo_jornalismo();
-        $dados['outras_noticias']   = $this->Novomenina->outras_noticias();
+        $dados['titulo']            = $this->Novomenina->noticias_turistmo_destaque($regiao);
+        $dados['eventos']           = $this->Novomenina->eventos($regiao);
+        $dados['programacao']       = $this->Novomenina->programacao($regiao);
+        $dados['videos']            = $this->Novomenina->videos($regiao);
+        $dados['titulo_jornalismo'] = $this->Novomenina->titulo_jornalismo($regiao);
+        $dados['outras_noticias']   = $this->Novomenina->outras_noticias($regiao);
         $this->load->view('Template', $dados);
     }
 
@@ -42,9 +43,9 @@ class home extends CI_Controller {
 
     public function programacao() {
         $dados['viewName'] = 'programacao';
-        $dados['titulo_jornalismo'] = $this->Novomenina->titulo_jornalismo();
-        $dados['programacao_impar'] = $this->Novomenina->programacao_impar();
-        $dados['programacao_par']   = $this->Novomenina->programacao_par(); 
+        $dados['titulo_jornalismo'] = $this->Novomenina->titulo_jornalismo($_SESSION['regiao'] );
+        $dados['programacao_impar'] = $this->Novomenina->programacao_impar($_SESSION['regiao'] );
+        $dados['programacao_par']   = $this->Novomenina->programacao_par($_SESSION['regiao'] ); 
         $this->load->view('Template', $dados);
 
     }
@@ -105,9 +106,9 @@ class home extends CI_Controller {
 
     public function noticia() {
         $categoria = $_GET['categoria'];
-        $dados['jornalismo_impar'] = $this->Novomenina->jornalismo_impar($categoria);
-        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo();
-        $dados['mais_lidas'] = $this->Novomenina->mais_lidas($categoria);
+        $dados['jornalismo_impar'] = $this->Novomenina->jornalismo_impar($categoria, $_SESSION['regiao']);
+        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo($_SESSION['regiao']);
+        $dados['mais_lidas'] = $this->Novomenina->mais_lidas($categoria, $_SESSION['regiao']);
         $dados['viewName'] = 'jornalismo/noticia';
         $this->load->view('Template', $dados);
     }
@@ -115,10 +116,10 @@ class home extends CI_Controller {
     public function descricao_noticia() {
         $id = $_GET['id'];
         $categoria = $_GET['categoria'];
-        $this->Novomenina->cliques($id);
-        $dados['descricao_noticia'] = $this->Novomenina->descricao_noticia($id);
-        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo();
-        $dados['mais_lidas'] = $this->Novomenina->mais_lidas($categoria);
+        $this->Novomenina->cliques($id, $_SESSION['regiao']);
+        $dados['descricao_noticia'] = $this->Novomenina->descricao_noticia($id, $_SESSION['regiao']);
+        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo($_SESSION['regiao']);
+        $dados['mais_lidas'] = $this->Novomenina->mais_lidas($categoria, $_SESSION['regiao']);
         $dados['viewName'] = 'jornalismo/descricao_noticia';
         $this->load->view('Template', $dados);
     }
@@ -127,25 +128,25 @@ class home extends CI_Controller {
     
 
     public function artistico() {
-        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo();
+        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo($_SESSION['regiao']);
         $dados['viewName'] = 'artistico';
         $this->load->view('Template', $dados);
     }
 
     public function eventos() {
-        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo();
+        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo($_SESSION['regiao']);
         $dados['viewName'] = 'eventos';
         $this->load->view('Template', $dados);
     }
 
     public function promocoes() {
-        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo();
+        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo($_SESSION['regiao']);
         $dados['viewName'] = 'promocoes';
         $this->load->view('Template', $dados);
     }
 
     public function quem_somos() {
-        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo();
+        $dados['titulo_jornalismo']= $this->Novomenina->titulo_jornalismo($_SESSION['regiao']);
         $dados['viewName'] = 'quem_somos';
         $this->load->view('Template', $dados);
     }
--- application/models/Novomenina_model.php
+++ application/models/Novomenina_model.php
@@ -8,62 +8,65 @@ class Novomenina_model extends CI_Model{
         $this->load->database();
     }
 
-    public function noticias_turistmo_destaque() {
-        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.destaque = 1 and noticias.mostrar = 1 ORDER BY data desc limit 3");
+    public function noticias_turistmo_destaque($regiao) {
+        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.destaque = 1 and noticias.mostrar = 1 and noticias.regiao = '$regiao' ORDER BY data desc limit 3");
         return $query->result_array();
     }
 
-    public function outras_noticias() {
-        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.destaque = 0 and noticias.mostrar = 1 ORDER BY data desc limit 4;");
+    public function outras_noticias($regiao)  {
+        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.destaque = 0 and noticias.mostrar = 1 and noticias.regiao = '$regiao' ORDER BY data desc limit 4;");
         return $query->result_array();
     }
 
-    public function eventos() {
+    public function eventos($regiao)  {
+        $this->db->where('regiao', '$regiao');
         $query = $this->db->get('eventos');
         return $query->result_array();
     }
 
-    public function programacao() {
+    public function programacao($regiao) {
+        $this->db->where('regiao', '$regiao');
         $query = $this->db->get('programacao');
         return $query->result_array();
     }
 
-    public function programacao_impar() {
-        $query = $this->db->query('SELECT * FROM programacao WHERE cod % 2 != 0');
+    public function programacao_impar($regiao)  {
+        $query = $this->db->query("SELECT * FROM programacao WHERE cod % 2 != 0 and programacao.regiao = '$regiao' ");
         return $query->result_array();
     }
 
-    public function programacao_par() {
-        $query = $this->db->query('SELECT * FROM programacao WHERE cod % 2 = 0');
+    public function programacao_par($regiao)  {
+        $query = $this->db->query("SELECT * FROM programacao WHERE cod % 2 = 0 and programacao.regiao = '$regiao'");
         return $query->result_array();
     }
 
-    public function videos() {
+    public function videos($regiao)  {
+        $this->db->where('regiao', '$regiao');
         $query = $this->db->get('videos');
         return $query->result_array();
     }
 
-    public function titulo_jornalismo() {
-        $query = $this->db->query('SELECT DISTINCT categorias.categoriaPt, noticias.codCategoria from categorias inner join noticias WHERE categorias.cod = noticias.codCategoria');
+    public function titulo_jornalismo($regiao)  {
+        $query = $this->db->query("SELECT DISTINCT categorias.categoriaPt, noticias.codCategoria from categorias inner join noticias WHERE categorias.cod = noticias.codCategoria and noticias.regiao = '$regiao' ");
         return $query->result_array();
     }
 
-    public function jornalismo_impar($categoria) {
-        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.destaque = 1 and noticias.mostrar = 1 and categoriaPt = '$categoria' ORDER BY data desc");
+    public function jornalismo_impar($categoria, $regiao) {
+        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.destaque = 1 and noticias.mostrar = 1 and categoriaPt = '$categoria'  and noticias.regiao = '$regiao' ORDER BY data desc");
         return $query->result_array();
     }
 
-    public function descricao_noticia($id) {
-        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.destaque = 1 and noticias.mostrar = 1 and noticias.cod = $id ORDER BY data DESC");
+    public function descricao_noticia($id, $regiao) {
+        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.destaque = 1 and noticias.mostrar = 1 and noticias.cod = $id  and noticias.regiao = '$regiao' ORDER BY data DESC");
         return $query->result_array();
     }
 
-    public function cliques($id) {
-        $query = $this->db->query("UPDATE noticias SET cliques = cliques + 1 WHERE cod = $id; ");
+    public function cliques($id, $regiao) {
+        $query = $this->db->query("UPDATE noticias SET cliques = cliques + 1 WHERE cod = $id  and noticias.regiao= '$regiao'");
     }
 
-    public function mais_lidas($categoria) {
-        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.mostrar = 1 and categoriaPt = '$categoria' ORDER BY cliques desc");
+    public function mais_lidas($categoria, $regiao) {
+        $query = $this->db->query("SELECT noticias.*, categorias.categoriaPt FROM noticias INNER JOIN categorias WHERE categorias.cod = noticias.codCategoria and noticias.mostrar = 1 and categoriaPt = '$categoria'  and noticias.regiao= '$regiao' ORDER BY cliques desc");
         return $query->result_array();
     }
 }
