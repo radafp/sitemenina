ae045c16cc1277b66a7e4076346282bcb8418505
radamesprovin <radafp@gmail.com>
2017-12-14 07:06:25 -0200

    ajuste nome imagens Hmome

--- application/views/home.php
+++ application/views/home.php
@@ -40,7 +40,7 @@
                 <div class="col-lg-4 mb-4">
                     <div class="box-regiao">
                         <div class="logoRegiao">
-                            <img src="<?php echo base_url('/assets/img/logoMeninaBC.png')?>" title="Radio Menina Balneário Camboriú">
+                            <img src="<?php echo base_url('/assets/img/logoMeninabc.png')?>" title="Radio Menina Balneário Camboriú">
                         </div>
                         <p>Balneário Camboriú</p>
                         <a href="home/balneario_camboriu">Acessar</a>
@@ -49,7 +49,7 @@
                 <div class="col-lg-4 mb-4">
                     <div class="box-regiao">
                         <div class="logoRegiao">
-                            <img src="<?php echo base_url('/assets/img/logoMeninaBL.png')?>" title="Radio Menina Blumenau">
+                            <img src="<?php echo base_url('/assets/img/logoMeninabl.png')?>" title="Radio Menina Blumenau">
                         </div>
                         <p>Blumenau</p>
                         <a href="Blumenau">Acessar</a>
@@ -58,7 +58,7 @@
                 <div class="col-lg-4 mb-4">
                     <div class="box-regiao">
                         <div class="logoRegiao">
-                            <img src="<?php echo base_url('/assets/img/logoMeninaLG.png')?>" title="Radio Menina Lages">
+                            <img src="<?php echo base_url('/assets/img/logoMeninalg.png')?>" title="Radio Menina Lages">
                         </div>
                         <p>Lages</p>
                         <a href="Lages" name='lages'>Acessar</a>
