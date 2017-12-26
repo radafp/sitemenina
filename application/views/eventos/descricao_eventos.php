79454a64ec51243a853f7c58ff8ff70c646849c4
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-12-26 20:02:41 -0200

    querys alteradas de jornalismo/promocoes/eventos

--- application/views/eventos/descricao_eventos.php
+++ application/views/eventos/descricao_eventos.php
@@ -1,15 +1,18 @@
 <div class='programacao'>
-    <div class='programacoes'>
-        <h2>Descrição da Notícia</h2><br>
-        <div id='prog'>
-            <?php foreach($descricao_eventos as $info):?>
-            <div id='esquerda'>
-                <h3><?php echo $info['tituloPt'].'<br>'?></h3>
-                <p><?php echo 'Início: '. date('d/m/Y', strtotime($info['dataInicio'])). '<br>' . $info['descricaoPt'].'<br>'.$info['mapa']?></p>
-            </div>
-            <?php endforeach?>
+<div class='programacoes'>
+    <h2>Descrição da Promoção</h2><br>
+    <div id='prog'>
+        <?php foreach($descricao_eventos as $info):?>
+        <div id='esquerda'><br>
+            <h3><?php echo $info['tituloPt'].'<br>'?></h3>
+            <p><?php echo $info['descricaoPt']?></p>
+            <p><?php echo 'Início: '. date('d/m/Y', strtotime($info['dataInicio'])). '<br>' . $info['descricaoPt'].'<br>'?></p>
+            <button>REGULAMENTO</button>
+            <BUtton>QUERO PARTICIPAR</BUtton>
         </div>
+        <?php endforeach?>
     </div>
+</div>
 
 </div><br><br><br>
 <?php 
--- application/views/promocoes/promocoes.php
+++ application/views/promocoes/promocoes.php
@@ -1,11 +1,44 @@
+<script src="<?php echo base_url('/assets/js/popper.min.js')?>"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+<script type="text/javascript">
+
+    $(document).ready(function(){
+
+        var content = $('#content');
+        $('.link_descricao').click(function( e ){
+            e.preventDefault();
+
+            var href = $( this ).attr('href');
+            $.ajax({
+                url: href,
+                success: function( response ){
+                    //forçando o parser
+                    var data = $( '<div>'+response+'</div>' ).find('#content').html();
+
+                    //apenas atrasando a troca, para mostrarmos o loading
+                    window.setTimeout( function(){
+                        content.fadeOut('fast', function(){
+                            content.html( data ).fadeIn();
+                        });
+                    },100);
+                }
+            });
+        });
+    });
+</script>
+
 <div class='programacao'>
     <div class='programacoes'>
         <h2>Promoções</h2><br><br>
         <div id='prog'>
             <?php foreach($promocoes_impar as $info):?>
                 <div id='esquerda'>
-                <a href="<?php echo base_url('home/descricao_promocoes?id='.$info['cod'].'&regiao='.strtolower($info['regiao']))?>">
-                    <img src="<?php echo base_url('/assets/img/logoTVMocinha.png')?>" alt="" width='680px'>
+                <a class='link_descricao' href="<?php echo base_url('home/descricao_promocoes?id='.$info['cod'].'&regiao='.strtolower($info['regiao']))?>">
+                <img src="<?php echo base_url('assets/arquivos/programacao/'.$info["arquivo"])?>" alt="" width='680px'>
                     <h3><?php echo $info['tituloPt']?></h3>
                     <!-- <p><?php echo $info['descricao']?></p> -->
                     <span><?php echo 'Início:'.$info['dataInicio']. "<br>Fim:". $info['dataFim']?></span><br>
