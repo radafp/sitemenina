e0c0c3dfe7757e6ff5360567cc46283e1a840fbb
atendimentoSet <atendimentoset@gmail.com>
2018-01-04 16:10:08 -0200

    alteracao tamanho

--- application/views/programacao/programacao.php
+++ application/views/programacao/programacao.php
@@ -97,9 +97,6 @@
 
             </div> <!-- contLeft -->
             <div class="col-xs-12 col-md-4 contRight">
-                <?php foreach($banner_tipo3 as $info):?>
-                    <a href=<?= $info['link']?> target='__blank'><img src=<?= base_url('/assets/arquivos/publicidade/'.$info['arquivo'])?> title="Publicidade"></a>
-                <?php endforeach?>
                 <?php foreach($banner_tipo2 as $info):?>
                     <a href=<?= $info['link']?> target='__blank'><img src=<?= base_url('/assets/arquivos/publicidade/'.$info['arquivo'])?> title="Publicidade"></a>
                 <?php endforeach?>
