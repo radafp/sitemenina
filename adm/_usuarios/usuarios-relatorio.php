7d432dd627b1abf7f8ec70d437bef8fa52687844
Dionathan Cordova <dionathan_bass@hotmail.com>
2018-03-01 09:58:42 -0300

    alteracao de relatorio usuarios

--- adm/_usuarios/usuarios-relatorio.php
+++ adm/_usuarios/usuarios-relatorio.php
@@ -13,13 +13,13 @@ $dataInicio = isset($_GET['dataInicio']) ? $_GET['dataInicio'] : '';
 $dataFim = isset($_GET['dataFim']) ? $_GET['dataFim'] : '';
 
 // formatando o valor que vem do input
-$dI1 = substr($dataInicio, 0, 10);
-$dI2 = substr($dataInicio, 11);
-$dataInicio = $dI1 . ' ' . $dI2 . ':00';
+// $dI1 = substr($dataInicio, 0, 10);
+// $dI2 = substr($dataInicio, 11);
+// $dataInicio = $dI1 . ' ' . $dI2 . ':00';
 
-$dF1 = substr($dataFim, 0, 10);
-$dF2 = substr($dataFim, 11);
-$dataFim = $dF1 . ' ' . $dF2 . ':00';
+// $dF1 = substr($dataFim, 0, 10);
+// $dF2 = substr($dataFim, 11);
+// $dataFim = $dF1 . ' ' . $dF2 . ':00';
 
 
 
@@ -35,9 +35,12 @@ if($usuario)
 
 	$conn = new PDO('mysql:host=mysql03-farm70.uni5.net;dbname=novomenina', 'novomenina', 'agEncia445');
 	
-	if(isset($dataInicio) && $dataInicio != ':00' && $dataInicio != '2018-01-01 08:30:00' && isset($dataFim) && $dataFim != ':00' && $dataFim != '2018-01-01 08:30:00') {
+	if(isset($dataInicio) && !empty($dataInicio) && isset($dataFim) && !empty($dataFim)) {
+		$dataInicio .= ' 00:00:00';
+		$dataFim 	.= ' 23:59:59';
 		$dataconsulta = "And usuariosStats.dataCadastro BETWEEN '$dataInicio' AND '$dataFim' ";
-
+		// echo '<br>BT: ' . $dataInicio . ' -- ' . $dataFim;
+		
 		$data = $conn->query(
 			"SELECT usuariosStats.dataCadastro, 
 					usuariosStats.acao,
@@ -52,9 +55,10 @@ if($usuario)
 		);
   
 	// se for setado somente a data inicial na pesquisa
-	}elseif(isset($dataInicio) && $dataInicio != ':00' && $dataInicio != '2018-01-01 08:30:00') {
+	}elseif(isset($dataInicio) && !empty($dataInicio)) {
+		$dataInicio .= ' 00:00:00';
 		$dataconsulta = "And usuariosStats.dataCadastro >= '$dataInicio' ";
-
+		// echo '<br>DI: ' . $dataInicio;
 		$data = $conn->query(
 			"SELECT usuariosStats.dataCadastro, 
 					usuariosStats.acao,
@@ -69,9 +73,10 @@ if($usuario)
 		);	
 
 	// se for setado somente a data Final na pesquisa
-	}elseif(isset($dataFim) && $dataFim != ':00' && $dataFim != '2018-01-01 08:30:00') {
+	}elseif(isset($dataFim) && !empty($dataFim)) {
+		$dataFim 	.= ' 23:59:59';
 		$dataconsulta = "And usuariosStats.dataCadastro <= '$dataFim' ";
-
+		// echo '<br>DF: '.$dataFim;
 		$data = $conn->query(
 			"SELECT usuariosStats.dataCadastro, 
 					usuariosStats.acao,
@@ -83,6 +88,25 @@ if($usuario)
 			inner join usuarios
 				on usuarios.cod = usuariosStats.codUsuario
 				and usuariosStats.codUsuario = $cod " . $dataconsulta
+		);
+	}
+
+	// echo '<br><br><br>disemnada:'.$dataInicio;
+
+	// query trazendo tudo do usuario selecionado
+	if(empty($dataInicio) && empty($dataFim)) {
+		// echo 'vazio';
+		$data = $conn->query(
+			"SELECT usuariosStats.dataCadastro, 
+					usuariosStats.acao,
+					usuariosStats.nomeMenu,
+					(SELECT COUNT(usuariosStats.acao) AS countAcao
+					FROM usuariosStats
+							where usuariosStats.codUsuario = usuarios.cod) as countAcao
+				FROM usuariosStats
+			inner join usuarios
+				on usuarios.cod = usuariosStats.codUsuario
+				and usuariosStats.codUsuario = $cod "
 		);
 	}
 	
